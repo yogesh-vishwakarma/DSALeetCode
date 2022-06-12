719684268e7e29f3036f77e2a64404d5d10d930a
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-12 12:47:20 +0530

    Time: 3 ms (38.18%), Space: 5.8 MB (65.30%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int a=1;
-        while(num>0){
-            num-=a;
-            a+=2;
+        // int a=1;
+        // while(num>0){
+        //     num-=a;
+        //     a+=2;
+        // }
+        // return(num==0);
+        
+        int left = 1, right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // to avoid overflow incase (left+right)>2147483647
+            int res = num / mid, remain = num % mid;
+            if (res == mid && remain == 0) return true; // check if mid * mid == num
+            if (res > mid) { // mid is small -> go right to increase mid
+                left = mid + 1;
+            } else {
+                right = mid - 1; // mid is large -> to left to decrease mid
+            }
         }
-        return(num==0);
+        return false;
     }
 };
