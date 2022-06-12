302b810068c0dd28b339263e98335ec8e075611a
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-12 18:22:08 +0530

    Time: 167 ms (99.15%), Space: 89.6 MB (96.48%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>& nums) {
-        int result = 0;
-        unordered_set<int> hset;
-        for (int i = 0, j = 0, win = 0; j < nums.size(); j++) {
-            while (hset.find(nums[j]) != hset.end()) {
-                hset.erase(nums[i]);
-                win -= nums[i];
-                i++;
+    int maximumUniqueSubarray(const vector<int>& nums) {
+                
+        int left = -1;
+        int max_sum = 0;
+        int sum = 0;
+        
+        array<int, 100001> prefix_sums = {};
+        array<int, 10001> idxs;
+        fill(begin(idxs), end(idxs), -1);
+        
+        for (int cur = 0; cur < nums.size(); ++cur) {            
+            prefix_sums[cur] = nums[cur] + (cur > 0 ? prefix_sums[cur - 1] :0);
+            if (idxs[nums[cur]] != -1) {
+                left = max(left, idxs[nums[cur]] );
+            //  cout << nums[cur] << " was already seeen\n";
             }
-            hset.insert(nums[j]);
-            win += nums[j];
-            result = max(result, win);
+
+            idxs[nums[cur]] = cur;
+
+            sum =  prefix_sums[cur] - (left != -1 ? prefix_sums[left]   :0);
+            max_sum = max(max_sum, sum);
+            
         }
-        return result;
+        
+        return max_sum;
+        
     }
 };
