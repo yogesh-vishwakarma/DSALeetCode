46fa945dd5fe4f5cc3ba458b36971c42d982a140
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-13 09:51:53 +0530

    Time: 4 ms (90.07%), Space: 7.1 MB (95.92%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,55 +1,35 @@
 class Solution {
-public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> result;
-        string row(n,'.');
-        vector<string> board(n,row);
-        
-        backtracking(n, result, board, 0);
-        return result;
+  public:
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, vector < int > & leftrow, vector < int > & upperDiagonal, vector < int > & lowerDiagonal, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
+          board[row][col] = 'Q';
+          leftrow[row] = 1;
+          lowerDiagonal[row + col] = 1;
+          upperDiagonal[n - 1 + col - row] = 1;
+          solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
+          board[row][col] = '.';
+          leftrow[row] = 0;
+          lowerDiagonal[row + col] = 0;
+          upperDiagonal[n - 1 + col - row] = 0;
+        }
+      }
     }
-    
-    void backtracking(int n, vector<vector<string>> &ans, vector<string> &board, int col){
-        if(col == n){
-            ans.push_back(board);
-            return;
-        }
-        
-        for(int row(0);row<n;row++){
-            if(isSafe(row, col, board, n)){
-                board[row][col] = 'Q';
-                backtracking(n, ans, board, col+1);
-                board[row][col] = '.';
-            }
-        }
-    }
-    
-    bool isSafe(int row, int col ,vector<string> board,int n){
-        int duprow = row;
-        int dupcol = col;
-        
-        
-        // check upper diagonal
-        while(row>=0 && col >= 0){
-            if(board[row--][col--] == 'Q')
-                return false;
-        }
-        
-        // check row's prev records
-        col = dupcol; row = duprow;
-        while(col >= 0){
-            if(board[row][col--] == 'Q')
-                return false;
-        }
-        
-        // check lower diagonal
-        col = dupcol; row = duprow;
-        while( col>=0 && row <n){
-            if(board[row++][col--] == 'Q')
-                return false;
-        }
-        
-        
-        return true;
+
+  public:
+    vector < vector < string >> solveNQueens(int n) {
+      vector < vector < string >> ans;
+      vector < string > board(n);
+      string s(n, '.');
+      for (int i = 0; i < n; i++) {
+        board[i] = s;
+      }
+      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
+      solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
+      return ans;
     }
 };
