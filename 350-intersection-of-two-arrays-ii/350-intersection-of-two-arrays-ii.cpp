650f3b8452493c0f39214a385a6850717fa84b4e
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-18 18:55:51 +0530

    Time: 8 ms (58.05%), Space: 10.9 MB (5.61%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,41 +1,34 @@
 class Solution {
-    bool find(vector<int>&nums, int val) {
-		int s = 0;
-		int e = nums.size()-1;
-		
-		while (s <= e) {
-			int mid = s + (e-s)/2;
-			if (nums[mid] == val) {
-				nums.erase(nums.begin()+mid);
-				return true;
-			} else if (nums[mid] < val)
-				s = mid+1;
-			else
-				e = mid-1;
-		}
-		
-		return false;
-	}
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-			sort(nums1.begin(), nums1.end());
-			sort(nums2.begin(), nums2.end());
-			
-			int l1 = nums1.size();
-			int l2 = nums2.size();
-			
-			vector<int>ans;
-			
-			if (l1 < l2) {
-				for (int i = 0; i < l1; i++)
-					if (find(nums2, nums1[i]))
-						ans.push_back(nums1[i]);
-			} else {
-				for (int i = 0; i < l2; i++)
-					if (find(nums1, nums2[i]))
-						ans.push_back(nums2[i]);
-			}
-			
-			return ans;
+        unordered_map<int,int> map1;
+        unordered_map<int,int> map2;
+        vector<int> result;
+
+        for(int i=0;i<nums1.size();i++){
+
+          if (map1.find(nums1[i]) == map1.end())
+            map1[nums1[i]]=1;
+          else 
+            map1[nums1[i]]++;
+        }
+
+        for(int i=0;i<nums2.size();i++){
+
+          if (map2.find(nums2[i]) == map2.end())
+            map2[nums2[i]]=1;
+          else 
+            map2[nums2[i]]++;
+        }
+
+        for(auto itr:map1){
+          if(map2.find(itr.first) != map2.end()){
+            int i=min(map2[itr.first], map1[itr.first]);
+            while(i--){
+              result.push_back(itr.first);
+            }
+          }
+        }
+        return result;
     }
 };
