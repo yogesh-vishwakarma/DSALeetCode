9c73163c6b29a9757f468dba5d9ef9190e930782
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-20 08:50:59 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (29.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -4,8 +4,23 @@ public:
         int lo = 0, hi = nums.size();
         while (lo < hi) {
             int mid = (lo + hi) / 2;
+/*
+Here we are choosing min value 
 
-            double num = (nums[mid] < nums[lo]) == (target < nums[lo]) ? nums[mid] : (target < nums[lo] ? -INFINITY : INFINITY);
+*/
+            // double num = (nums[mid] < nums[lo]) == (target < nums[lo]) ?
+            //             nums[mid] : 
+            //             (target < nums[lo] ? -INFINITY : INFINITY);
+            
+            double num;
+            if(nums[mid] < nums[lo] == target < nums[lo])
+                num = nums[mid];
+            else{
+                if(target < nums[lo])
+                    num = -INFINITY;
+                else
+                    num = INFINITY;
+            }
 
             if (num < target)
                 lo = mid + 1;
