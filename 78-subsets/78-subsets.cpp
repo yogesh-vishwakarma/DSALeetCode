b87ccc5710bc870d68c4d8a846336c35ccee3ea7
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-22 21:44:55 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (99.37%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -16,30 +16,30 @@ public:
 //         }
 //     }
     
-    // iterative
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs = {{}};
-        for (int num : nums) {
-            int n = subs.size();
-            for (int i = 0; i < n; i++) {
-                subs.push_back(subs[i]); 
-                subs.back().push_back(num);
-            }
-        }
-        return subs;
-    }
-    
-    // bitwise
+    /*iterative*/
     // vector<vector<int>> subsets(vector<int>& nums) {
-    //     int n = nums.size(), p = 1 << n;
-    //     vector<vector<int>> subs(p);
-    //     for (int i = 0; i < p; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if ((i >> j) & 1) {
-    //                 subs[i].push_back(nums[j]);
-    //             }
+    //     vector<vector<int>> subs = {{}};
+    //     for (int num : nums) {
+    //         int n = subs.size();
+    //         for (int i = 0; i < n; i++) {
+    //             subs.push_back(subs[i]); 
+    //             subs.back().push_back(num);
     //         }
     //     }
     //     return subs;
     // }
+    
+    /*bitwise*/
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
 };
