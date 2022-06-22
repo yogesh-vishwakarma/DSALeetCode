ee7ca54997eb9ecd83b4faa353000e50405331d9
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-22 21:47:50 +0530

    Time: 114 ms (58.33%), Space: 44 MB (10.43%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>mp;
-        int count = 0;
-        for(auto e : tasks)
-        {
-            mp[e]++;
-            count = max(count, mp[e]);
-        }
+//     int leastInterval(vector<char>& tasks, int n) {
+//         unordered_map<char,int>mp;
+//         int count = 0;
+//         for(auto e : tasks)
+//         {
+//             mp[e]++;
+//             count = max(count, mp[e]);
+//         }
         
-        int ans = (count-1)*(n+1);
-        for(auto e : mp) if(e.second == count) ans++;
-        return max((int)tasks.size(), ans);
+//         int ans = (count-1)*(n+1);
+//         for(auto e : mp) if(e.second == count) ans++;
+//         return max((int)tasks.size(), ans);
+//     }
+    
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> counts;
+        for (char t : tasks) {
+            counts[t]++;
+        }
+        priority_queue<int> pq;
+        for (pair<char, int> count : counts) {
+            pq.push(count.second);
+        }
+        int alltime = 0;
+        int cycle = n + 1;
+        while (!pq.empty()) {
+            int time = 0;
+            vector<int> tmp;
+            for (int i = 0; i < cycle; i++) {
+                if (!pq.empty()) {
+                    tmp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for (int cnt : tmp) {
+                if (--cnt) {
+                    pq.push(cnt);
+                }
+            }
+            alltime += !pq.empty() ? cycle : time;
+        }
+        return alltime;
     }
 };
