007f8f4059d58d0f6162ac4df5e52b1ce3092691
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-24 20:21:02 +0530

    Time: 67 ms (34.37%), Space: 19.5 MB (42.74%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -29,21 +29,14 @@ public:
         stack<pair<int,int>> st;
         st.push({intervals[0][0],intervals[0][1]});
         int n=intervals.size();
-        for(int i=1;i<n;i++)
-        {
+        for(int i=1;i<n;i++){
             if(st.top().second>=intervals[i][0])
-            {
                 st.top().second=intervals[i][1]>st.top().second? intervals[i][1]:st.top().second; 
-                // In case of [1,10],[2,9]            
-            } 
             else
-               {
-                   st.push({intervals[i][0],intervals[i][1]});
-               }   
+               st.push({intervals[i][0],intervals[i][1]});
         }
         vector<int> p;
-        while(!st.empty())
-        {
+        while(!st.empty()){
             p.push_back(st.top().first);
             p.push_back(st.top().second);
             ans.push_back(p);
