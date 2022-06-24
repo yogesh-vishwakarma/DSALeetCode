5ca29d5385f36632c038e3292e22d27039b82683
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-24 20:45:21 +0530

    Time: 44 ms (28.79%), Space: 24 MB (81.83%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> output(nums.size(),1);
-        for (int i = 1; i < nums.size(); ++i){
-            output[i] = output[i-1] * nums[i-1];
+    vector<int> productExceptSelf(vector<int> nums) {
+        // vector<int> output(nums.size(),1);
+        // for (int i = 1; i < nums.size(); ++i){
+        //     output[i] = output[i-1] * nums[i-1];
+        // }
+        // for (int i = nums.size() - 2; i >= 0; --i){
+        //     nums[i] *= nums[i + 1];
+        //     output[i] *= nums[i+1];
+        // }
+        // return output;
+        
+        /* Method 2 */
+        int mul = 1, zeroCnt(0);
+        for(auto el: nums){
+            if(el == 0)
+                zeroCnt++;
+            else
+                mul *=el;
         }
-        for (int i = nums.size() - 2; i >= 0; --i){
-            nums[i] *= nums[i + 1];
-            output[i] *= nums[i+1];
+        for(int i = 0;i<nums.size();i++){
+            if(zeroCnt){
+                if(nums[i]==0 && zeroCnt>1)
+                    nums[i] = 0;
+                else if(nums[i]==0)
+                    nums[i] = mul;
+                else
+                    nums[i] = 0;
+            }else{
+                nums[i] = mul/nums[i];
+            }
         }
-        return output;
+        return nums;
+        
     }
 };
