7902b8407fc00cc7f281460316df20b9477e64f0
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-25 19:42:00 +0530

    Time: 155 ms (67.76%), Space: 61.3 MB (61.15%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -19,6 +19,19 @@ public:
         return low;
     }
     
+    
+    /*
+    Because the indexes are in range (1,n) and elements also must be in range (1,n);
+    we can see that simillar elements have diffrent index, diffrent index have simillar elements
+    so if we use elements as indexes we will come for same index multiple times.
+    so we can  see it as a circular path.
+    exx:
+    0, 1, 2, 3, 4
+    3, 1, 3, 4, 2
+    
+    0th idex 3-> 3rd index 4-> 4th index 2-> 2nd index 3->3rd index 4-> 4th index 2 (looping)
+    it's like circular linked list, so can use that approach here.
+    */
     int findDuplicate(vector<int>& nums) {
         int len = nums.size();
         int slow = 0;
