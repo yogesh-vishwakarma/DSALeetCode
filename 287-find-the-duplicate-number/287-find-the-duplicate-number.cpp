816be4b541d1483d38e772f29770274a6fe13e71
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-25 19:42:35 +0530

    Time: 135 ms (80.37%), Space: 61.3 MB (61.15%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    int findDuplicate1(vector<int>& nums) {
-        int low = 1, high = nums.size() - 1;
-        
-        while(low <=  high){
-            int cnt = 0;
-            int mid = (low + high)/2;
-            for(int n : nums)
-                if(n <= mid)
-                    ++cnt;
-            // binary search on left
-            if(cnt <= mid)
-                low = mid + 1;
-            else
-            // binary search on right
-                high = mid - 1;
-        }
-        return low;
-    }
-    
-    
     /*
     Because the indexes are in range (1,n) and elements also must be in range (1,n);
     we can see that simillar elements have diffrent index, diffrent index have simillar elements
