f19b3ddd4c82056241ee6b1165adc73ddbd4be4c
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-26 19:10:40 +0530

    Time: 56 ms (13.01%), Space: 13.3 MB (93.37%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -18,6 +18,18 @@ public:
         }
         return true;
     }
+    bool biPartiteDFS(int node, vector<vector<int>>& adj, vector<int> &color){
+        for(auto it : adj[node]) {
+            if(color[it] == -1) {
+                color[it] = 1 - color[node];
+                if(!biPartiteDFS(it, adj, color))
+                    return false; 
+            } 
+            else if(color[it] == color[node]) 
+                return false; 
+        }
+        return true; 
+    }
     
     bool biPartiteBFS(int src, vector<vector<int>>& adj, vector<int> &color){
         queue<int> bfs;
@@ -37,17 +49,5 @@ public:
             }
         }
         return true;
-    }
-    
-    bool biPartiteDFS(int node, vector<vector<int>>& adj, vector<int> &color){
-        for(auto it : adj[node]) {
-            if(color[it] == -1) {
-                color[it] = 1 - color[node];
-                if(!biPartiteDFS(it, adj, color)) {
-                    return false; 
-                }
-            } else if(color[it] == color[node]) return false; 
-        }
-        return true; 
     }
 };
