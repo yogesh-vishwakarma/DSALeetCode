bf690d4a783af4d446a5e4d3f8df0bda33d343cd
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-26 18:59:29 +0530

    Time: 39 ms (36.28%), Space: 13.5 MB (55.86%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // TC: 
+    bool isBipartite(vector<vector<int>>& graph) {
+        int vCount = graph.size();
+        vector<int> color(vCount,-1);
+        
+        for(int i=0;i<vCount;i++){
+            if(color[i] != -1)// means already visited/colored
+                continue;
+            if(!biPartiteBFS(i,graph,color))
+                return false;
+            
+        }
+        return true;
+    }
+    
+    bool biPartiteBFS(int src, vector<vector<int>>& adj, vector<int> &color){
+        queue<int> bfs;
+        bfs.push(src);
+        color[src] = 1;
+        while(!bfs.empty()){
+            int curr = bfs.front();
+            bfs.pop();
+            
+            for(auto itr: adj[curr]){
+                if(color[itr] == -1){
+                    color[itr] = 1 - color[curr];//1->0 & 0->1 
+                    bfs.push(itr);
+                }
+                else if(color[itr] == color[curr])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
