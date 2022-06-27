5c0e7def0be1ce1a10bcafd54f971ded1cac920f
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-27 06:44:20 +0530

    Time: 18 ms (91.52%), Space: 17.9 MB (64.01%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingNumber1(vector<int>& nums) {
+        int sum(0), res, n(nums.size());
+        for(auto i : nums)
+            sum +=i;
+        res = (n*(n+1)/2) - sum;
+        return res;
+    }
+    /*  Cyclic Sort */
+    int missingNumber(vector<int>& nums) {
+      int start=0; int n = nums.size();
+        
+        while(start<n){
+            if(nums[start]<n && start!=nums[start]) // checking if no is present on index or not
+                swap(nums[nums[start]], nums[start]);
+            else
+                start++;         
+        }
+       for(int i=0;i<n;i++)
+           if(nums[i]!=i)
+               return i;
+        return 
+            n;
+    }
+};
