d4f00086bc47051e654da6c9ccc5c9b20740ee9c
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-06-29 07:27:57 +0530

    Time: 816 ms (7.76%), Space: 164.3 MB (96.24%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,120 +1,29 @@
-class Node{ // doubly linked list
+class LRUCache {
 public:
-    int key;
-    int val;
-    Node *next = NULL;
-    Node *prev = NULL;
-};
-
-class LRUCache{
-    private:
+    unordered_map <int, int> m, cnt;
+    queue <int> q;
+    int n;
+    LRUCache(int capacity) : n(capacity) {}
     
-    int cap; //max capacity of cache
-    int size = 0; // Curr size
-    Node *head = NULL; // initialize head of list to null
-    Node *tail = NULL; // intitialize tail to null
-    unordered_map<int, Node*> mp; //initialize hash map instanse
-
-    void updateNode(Node* &node){ // to update the Node
-        if (tail == node){
-            head->prev = tail;
-            tail->next = head;
-            head = tail;
-            tail = tail->prev;
-            tail->next = NULL;
-            return;
-        }
-        if (head == node){
-            return;
-        }
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-        head->prev = node;
-        node->prev = NULL;
-        node->next = head;
-        head = node;
-    }
-
-    void addNode(Node *node){ // to add new node in front of Doubly linked list
-        head->prev = node;
-        node->next = head;
-        head = node;
-        return;
-    }
-
-    public:
-        LRUCache(int capacity){ // constructore initialize cache capacity
-            cap = capacity;
-        }
-
-        int get(int key){ // get the val if found
-            auto it = mp.find(key);
-
-            if (it != mp.end()){
-                updateNode(it->second);
-                return it->second->val;
-            }
+    
+    int get(int key) {
+        if (cnt.find(key) == cnt.end()) 
             return -1;
+        q.push(key);
+        cnt[key]++;
+        return m[key];
+    }
+    
+    void put(int key, int value) {
+        q.push(key);
+        cnt[key]++;
+        m[key] = value;
+        while (cnt.size() > n) {
+            int cur = q.front(); q.pop();
+            if (cnt[cur]-- == 1) 
+                cnt.erase(cur);
         }
-
-        void put(int key, int value){
-            auto it = mp.find(key);
-            
-            if (it != mp.end()){
-                it->second->val = value;
-                updateNode(it->second);
-                return;
-            }
-
-    //         if head is null means list is empty
-            if (head == NULL)
-            {
-    //             create new node
-                Node *newNode = new Node;
-                newNode->key = key;
-                newNode->val = value;
-    //             add new node key,Node pair to hash map
-                mp.insert({ key,
-                    newNode });
-    //             assign newNode value to head
-                head = newNode;
-    //             assign newNode value to tail as there is only one node
-                tail = newNode;
-    //             increse number of element in cache
-                size++;
-                return;
-            }
-
-    //         if size is less then cache no need to delete lru item simply add new node to list
-            if (size < cap)
-            {
-    //             delcare new node
-                Node *newNode = new Node;
-                newNode->key = key;
-                newNode->val = value;
-
-    //             add new node key,Node pair to hash map
-                mp.insert({ key,
-                    newNode });
-    //             add node to list
-                addNode(newNode);
-    //             increse number of element in cache            
-                size++;
-                return;
-            }
-
-    // if cache is full remove element from tail lru
-            mp.erase(tail->key);
-            head->prev = tail;
-            tail->next = head;
-            head = tail;
-            tail = head->prev;
-            tail->next = NULL;
-            head->key = key;
-            head->val = value;
-            mp.insert({ key,
-                head });
-        }
+    }
 };
 
 /**
