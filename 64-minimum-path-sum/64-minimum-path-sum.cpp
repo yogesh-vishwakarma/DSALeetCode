376149c18e21539620ee2d12046e36c740e7d8e4
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-01 07:21:37 +0530

    Time: 14 ms (56.49%), Space: 9.6 MB (97.98%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    
     int minPathSum(vector<vector<int>>& grid) {
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(i==0 && j>0)
-                    grid[i][j] += grid[i][j-1];  
-                else if(i>0 && j>0)
-                  grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
-                if(j==0 && i>0)
-                    grid[i][j] += grid[i-1][j]; 
-                // cout << grid[i][j] <<" ";
+        int row = grid.size(), column = grid[0].size();
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(i==0 && j==0)
+                    continue;
+                if(i>0 && j==0) // first row (we can only go down)
+                    grid[i][j] +=grid[i-1][j];
+                else if(j>0 && i==0) // first col (we can only go right)
+                    grid[i][j] +=grid[i][j-1];
+                else // choose min path
+                    grid[i][j] += min(grid[i-1][j],grid[i][j-1]);
             }
-            // cout << endl;
         }
-        return grid[grid.size()-1][grid[0].size()-1];
+        return grid[row-1][column-1];
     }
 };
