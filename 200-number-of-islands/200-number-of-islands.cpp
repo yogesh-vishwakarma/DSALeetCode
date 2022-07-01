ac547f5d79354f3f86baea702de5aee41725a75f
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-01 09:16:05 +0530

    Time: 41 ms (80.71%), Space: 12.3 MB (58.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -5,16 +5,28 @@ public:
         for(int i(0);i<grid.size();i++){
             for(int j(0);j<grid[0].size();j++){
                 if(grid[i][j] == '1'){
-                    sinkIsland(grid,i,j);
+                    // sinkIslandBFS(grid,i,j);
+                    sinkIslandDFS(grid,i,j);
                     res++;
-                    grid[i][j] == '0';
+                    grid[i][j] = '0';
                 }
             }
         }
         return res;
     }
     
-    void sinkIsland(vector<vector<char>>& grid, int i, int j){
+    void sinkIslandDFS(vector<vector<char>> &grid, int i, int j){
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j]='0';
+        int dir[] = {-1,0,1,0,-1};
+        for(int k=0;k<4;k++)
+            sinkIslandDFS(grid,i+dir[k],j+dir[k+1]);
+    }
+    
+    void sinkIslandBFS(vector<vector<char>>& grid, int i, int j){
         int m = grid.size(), n = grid[0].size();
         int offsets[] = {0,1,0,-1,0}; //Offsets for visiting neighbouring elements. 
         //Represents [0,1],[1,0],[0,-1],[-1,0].
