6a6c56736e24757f3e8b32a89e060615b14d60ea
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-03 12:03:06 +0530

    Time: 48 ms (75.83%), Space: 89.4 MB (60.96%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2=nums2.size();
-        // int p1(0), p2(0);
-        // while(p1+p2 < (n1+n2)/2 ){
-        //     if(nums1[p1] < nums2[p2])
-        //       (p1<n1)? ++p1: ++p2;
+        // int n1 = nums1.size(), n2=nums2.size();
+        // int p1(0), p2(0), x=0, y=0;
+        // while(p1+p2 <= (n1+n2)/2 ){
+        //     y=x;
+        //     if( (p1 < n1 && nums1[p1] < nums2[p2]) || p2>= n2 )
+        //         x=nums1[p1++];
         //     else
-        //         (p2<n2)? ++p2: ++p1;
-        //     cout <<p1<<" "<<p2<<endl;
+        //         x=nums2[p2++];
         // }
-        // double ans = (nums1[p1]+nums2[p2])/2;
-        // return ans;
-        int j=0,k=0;
-        int x=0, y=0;
-        while(j+k<=((n1+n2)/2)){
-            if(k>=n2 || (j<n1 && nums1[j]<nums2[k])){
-                y=x;
-                x=nums1[j++];
-            }
-            else{
-                y=x;
-                x=nums2[k++];
-            }
-        }
-        if((n1+n2)%2==0)
-            return (double)(x+y)/2;
-        return (double)x;
         
+        
+        
+        int n1 = nums1.size(), n2=nums2.size();
+        int j=0,k=0, x=0, y=0;
+        while(j+k<=((n1+n2)/2)){
+                y=x;
+            (k>=n2 || (j<n1 && nums1[j]<nums2[k]))? x=nums1[j++] : x=nums2[k++];
+        }
+        return ((n1+n2)%2)? (double)x : (double)(x+y)/2;
     }
 };
