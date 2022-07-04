bb9f3ec6667b844f1fb56dae24bcce2313dd125a
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-04 21:03:11 +0530

    Time: 4 ms (31.38%), Space: 6.3 MB (79.98%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-   int MatchingPair(char c1, char c2)
-    {
-        if (c1 == '(' && c2 == ')' || c1 == '{' && c2 == '}' || c1 == '[' && c2 == ']')
-            return 1;
-        else
-            return 0;
-    }
-
-    bool isValid(string s) 
-    {    
-    stack<char> St;
-
-    for(int i=0;i<s.size();i++)
-    {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[' )
-        {   
-            St.push(s[i]);
-        }
+    
+    
+    bool isValid(string s) {
+        if(s.size()%2 != 0)
+            return false;
+        unordered_map<char,char> mp;
+        mp['[']=']'; mp['{']='}'; mp['(']=')';
         
-        else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
-        {   
-            if(St.empty()) return false;
-            else if (MatchingPair(St.top(),s[i])) St.pop();
-            else return false;
+        stack<char> st;
+        for(auto el :  s ){
+            if(el == '(' || el == '{' || el == '[')
+                st.push(el);
+            else{
+                if(!st.empty() && el == mp[st.top()])
+                    st.pop();
+                else
+                    return false;
+            }
         }
-    } 
-    if (St.empty())
-        return true;
-    else
+        if(st.empty())
+            return true;
         return false;
     }
+    
 };
