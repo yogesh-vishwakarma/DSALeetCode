0fade258b4b0c91cefc1a41be4c654badaaced5d
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-05 08:58:09 +0530

    Time: 11 ms (49.35%), Space: 8.8 MB (57.70%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
-        stack<int> s;
-        unordered_map<int, int> m;
-        for (int n : nums) {
-            while (s.size() && s.top() < n) {
-                m[s.top()] = n;
-                s.pop();
-            }
-            s.push(n);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> ump;
+        stack<int> st;// int stPointer (0);
+        for(int el : nums2){
+            while( !st.empty() && st.top() < el){
+                ump[st.top()] = el; // here we are updating biggest element for all the smallest element from curr
+                st.pop();
+            } 
+            st.push(el);
         }
-        vector<int> ans;
-        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
-        return ans;
+        for(int i=0;i<nums1.size();i++){
+            nums1[i] = (ump.find(nums1[i])!=ump.end())? ump[nums1[i]] : -1;
+        }
+        return nums1;
     }
 };
