28b0c8fa73581d6b6098bda750c1c20d32a74183
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-06 08:38:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int dp[n+1]; // tabulation
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i<n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+    // recursion
+    int climbStairsR(int n) {
+        if(n < 3)
+            return n;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+    // memoization
+    int climbStairsM(int n){
+        vector<int> mem(n+1,-1);
+        return memoizationHelper(n,mem);
+    }
+    
+    int memoizationHelper(int n, vector<int> &mem) {
+        if(n < 3)
+            return n;
+        if(mem[n]!=-1)
+            return mem[n];
+        return mem[n] = memoizationHelper(n-1, mem) + memoizationHelper(n-2, mem);
+    }
+    // tabulation
+    int climbStairsT(int n){
+        vector<int> tab(n+1,-1);
+        tab[0]=1;tab[1]=2;
+        for(int i(2);i<n;i++){
+            tab[i] = tab[i-1] + tab[i-2];
         }
-        return dp[n-1];
+        return tab[n-1];
+    }
+
+    // space optimisation
+    int climbStairs(int n){
+        int prev2 = 1, prev = 1;
+      for(int i=2; i<=n; i++){
+          int cur_i = prev2+ prev;
+          prev2 = prev;
+          prev= cur_i;
+      }
+        return prev;
     }
 };
