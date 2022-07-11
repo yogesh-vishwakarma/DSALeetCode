32ae3c380233777a546098eb78bc1c7cb7a84724
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-11 09:11:41 +0530

    Time: 124 ms (83.06%), Space: 24.2 MB (90.58%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -5,26 +5,27 @@ public:
     
     If the current building is '0', then the number of sequences of pattern "101" will be the product of the number of occurances of building '1' before the current building to the number of occurances of building '1' after the current building and viceversa.
     
+    
+    we are getting total 0 & 1 counts
     */
     
     long long numberOfWays(string s) {
-        long long a=0,b=0,ans=0;        
+        long long totalOneCount=0,totalZeroCount=0, ans=0;        
+        for(auto curr : s)
+            (curr == '1')? totalOneCount++: totalZeroCount++;
         // a and b are the number of occurances of '1' and '0' after the current building respectively.
-        for(auto curr : s){
-            (curr == '1')? a++: b++;
-        }
-        long long c=0,d=0;              
-        // c and d are the number of occurances of '1' and '0' before the current building respectively.
+        
+        long long currOneCount=0,currZeroCount=0;
         for(auto el : s){
-             // Counting the sequences of "010"
+             // Counting the sequences of "010" = 
             if(el == '1'){
-                ans+= (d*b);
-                a--;c++;
+                ans+= (currZeroCount*totalZeroCount);
+                currOneCount++;totalOneCount--;// increasing curr count and reducing totalCount.
             }
             else{                        
                 // Counting the sequences of "101"
-                ans+=(a*c);
-                b--;d++;
+                ans+=(totalOneCount*currOneCount);
+                currZeroCount++;totalZeroCount--;
             }
         }
         return ans;
