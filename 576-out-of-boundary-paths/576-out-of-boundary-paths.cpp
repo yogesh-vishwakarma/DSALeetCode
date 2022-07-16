866be76c492e4185b5d6966134389dfee78cbb5d
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-16 06:47:49 +0530

    Time: 13 ms (64.10%), Space: 6 MB (99.80%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    int mod = pow(10,9) + 7;
-    int helperFunction(int row, int col, int maxMoves, int x, int y,  int moves, vector<vector<vector<int>>> & dp){
-        if(x < 0 || y < 0 || x >= row || y >= col){
-            return 1;
-        }
-        if(moves == maxMoves){
-            return 0;
-        }
-        if(dp[moves][x][y] != -1){
-            return dp[moves][x][y] % mod;
-        }
-        int tempVal = 0;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x, y - 1, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x - 1, y, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x + 1, y, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x, y + 1, moves + 1, dp)) % mod;
-        dp[moves][x][y] = tempVal;
-        return dp[moves][x][y] % mod;
-    }
     int findPaths(int m, int n, int N, int i, int j) {
-        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>> (m + 1, vector<int> (n + 1, -1)));
-        dp[N][m][n] = helperFunction(m, n, N, i, j, 0, dp);
-        return dp[N][m][n];
+        constexpr std::size_t MAX_BOARD_SIZE = 50;
+        constexpr int MOD_MAGIC_NUMBER = 1000000007;
+        
+        int sum = 0;
+        
+        // Build a board of with width and height of MAX N (50) plus out of bounds edges
+        // on each side.
+        std::array<std::array<int, MAX_BOARD_SIZE + 2>, MAX_BOARD_SIZE + 2> board{};
+        
+        // Place the ball at its initial position
+        board[i+1][j+1] = 1;
+        
+        // Progress board state N times
+        for (int x = 0; x < N; ++x) {
+            
+            // Make a new array that we will use as next board state
+            std::array<std::array<int, MAX_BOARD_SIZE + 2>, MAX_BOARD_SIZE + 2> new_board{};
+            
+            // For each number on the board, add it to each of its adjacent tiles
+            for (int k = 1; k < m + 1; ++k) {
+                for (int l = 1; l < n + 1; ++l) {
+                    int ball_count = board[k][l];
+                    if (ball_count == 0) { continue; }
+                    new_board[k-1][l  ] = (new_board[k-1][l  ] + ball_count) % MOD_MAGIC_NUMBER;
+                    new_board[k  ][l+1] = (new_board[k  ][l+1] + ball_count) % MOD_MAGIC_NUMBER;
+                    new_board[k+1][l  ] = (new_board[k+1][l  ] + ball_count) % MOD_MAGIC_NUMBER;
+                    new_board[k  ][l-1] = (new_board[k  ][l-1] + ball_count) % MOD_MAGIC_NUMBER;
+                }
+            }
+            
+            // Copy new board state over old board state
+            board = new_board;
+            
+            // Add out of bounds states to the running sum (mod the magic number)
+            for (int x = 0; x <= n  ; ++x) { sum = (sum + board[0  ][x  ]) % MOD_MAGIC_NUMBER; }
+            for (int x = 0; x <= m  ; ++x) { sum = (sum + board[x  ][n+1]) % MOD_MAGIC_NUMBER; }
+            for (int x = 1; x <= n+1; ++x) { sum = (sum + board[m+1][x  ]) % MOD_MAGIC_NUMBER; }
+            for (int x = 1; x <= m+1; ++x) { sum = (sum + board[x  ][0  ]) % MOD_MAGIC_NUMBER; }
+        }
+        
+        return sum;
     }
-    
 };
