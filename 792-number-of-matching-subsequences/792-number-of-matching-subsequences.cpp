1fa4d16ae41aea4ff21367ef23339067cbf888dc
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2022-07-20 08:51:13 +0530

    Time: 225 ms (83.68%), Space: 48.1 MB (62.91%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,33 +1,7 @@
-struct Node {
-    const string& word;
-    int index;
-    Node(const string &word, int index) : word(word), index(index) {}
-};
 class Solution {
 public:
-    int numMatchingSubseq1(const string& s, vector<string>& words) {
-        vector<Node> buckets[26];
-        for (string& word : words) {
-            char startingChar = word[0];
-            buckets[startingChar-'a'].emplace_back(word, 0);
-        }
-        int ans = 0;
-        for (char c : s) {
-            auto currBucket = buckets[c-'a'];
-            buckets[c-'a'].clear();
-            for (Node& node : currBucket) {
-                ++node.index; // Point to next character of node.word
-                if (node.index == node.word.size()) {
-                    ++ans;
-                } else {
-                    buckets[node.word[node.index]-'a'].push_back(node);
-                }
-            }
-        }
-        return ans;
-    }
     
-    int numMatchingSubseq(string S, vector<string>& words) {
+    int numMatchingSubseq2(string S, vector<string>& words) {
         vector<const char*> waiting[128];
         for (auto &w : words)
             waiting[w[0]].push_back(w.c_str());
@@ -39,4 +13,25 @@ public:
         }
         return waiting[0].size();
     }
+    int numMatchingSubseq (string S, vector<string>& words) {
+		vector<vector<int>> alpha (26);
+		for (int i = 0; i < S.size (); ++i) alpha[S[i] - 'a'].push_back (i);
+		int res = 0;
+
+		for (const auto& word : words) {
+			int x = -1;
+			bool found = true;
+
+			for (char c : word) {
+				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
+				if (it == alpha[c - 'a'].end ()) found = false;
+				else x = *it;
+			}
+
+			if (found) res++;
+		}
+
+		return res;
+	}
+
 };
