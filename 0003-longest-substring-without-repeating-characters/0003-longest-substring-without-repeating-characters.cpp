9b7c82240f32ddcbe58704a95c1c1cd071615a69
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2023-12-28 10:13:37 +0530

    Time: 11 ms (68.98%), Space: 7.9 MB (87.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int stringLength = s.size(), maxLength = 0;
-        vector<int> charAsciiIndex(128, -1);
-
-        if (stringLength < 2) {
-            return stringLength;
-        }
-
-        int startIdx = 0; // Starting index of the current substring
-
-        for (int idx = 0; idx < stringLength; idx++) {
-            if (charAsciiIndex[s[idx]] != -1) {
-                int lastIndex = charAsciiIndex[s[idx]];
-                startIdx = max(startIdx, lastIndex); // Update the start index
+       int size = s.size();
+        int maxLength = 0;
+        vector<int> charIndex(128, -1);
+        for(int left =0, right = 0; right < size; right ++){
+            if(charIndex[s[right]] >= left){
+                left = charIndex[s[right]] + 1;
             }
-            maxLength = max(maxLength, idx - startIdx + 1);
-            charAsciiIndex[s[idx]] = idx + 1; // Update the last index (+1)
+            charIndex[s[right]] = right;
+            maxLength = max(maxLength, right -left + 1);
         }
-
         return maxLength;
     }
 };
