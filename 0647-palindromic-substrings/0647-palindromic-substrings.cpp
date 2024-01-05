61b6ac23e8e920a84cb90e51690f86a0cb062755
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-01-05 08:44:40 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (86.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    int countSubstrings(string S) {
-        int len = S.length(), ans = 0;
-        for (int i = 0; i < len; i++) {
-            int j = i - 1, k = i;
-            while (k < len - 1 && S[k] == S[k+1]) k++;
-            ans += (k - j) * (k - j + 1) / 2, i = k++;
-            while (~j && k < len && S[k++] == S[j--]) ans++;
+    int countSubstrings(string str) {
+        int length = str.length(), count = 0; // Variable to store the count of palindromic substrings
+
+        for (int center = 0; center < length; center++) {
+            int left = center - 1;
+            int right = center;
+
+            // Expand around the center for consecutive identical characters
+            while (right < length - 1 && str[right] == str[right + 1]) {
+                right++;
+            }
+
+            // Count substrings with consecutive identical characters
+            count += (right - left) * (right - left + 1) / 2;
+
+            // Move the center to the right of consecutive identical characters
+            center = right++;
+
+            // Expand around the center for palindromes
+            while (left >= 0 && right < length && str[right] == str[left]) {
+                count++;
+                left--;
+                right++;
+            }
         }
-        return ans;
+
+        return count;
     }
 };
