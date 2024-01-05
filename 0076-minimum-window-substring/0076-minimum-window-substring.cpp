85ae3998d75892e5849b3c3eac646c06fca8c5c3
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-01-05 07:43:57 +0530

    Time: 7 ms (79.76%), Space: 8.2 MB (91.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+   string minWindow(string s, string t) {
+        vector<int> hashArr(128, 0);
+        for (char el : t) {
+            hashArr[el]++;
         }
-        return d==INT_MAX? "":s.substr(head, d);
-        // vector<int> hashArr(128, 0);
-        // for(char el: t){
-        //     hashArr[el]++;
-        // }
-        // int left = 0, validator = t.size();
-        // int subStrHead = 0, subStrTail = INT_MAX, strSize = s.size();
-        // for(int idx = 0; idx < strSize; idx++){
-        //     if(hashArr[s[idx]] > 0){
-        //         validator --;
-        //     }
-        //     hashArr[s[idx]] --;
-        //     while(validator == 0 ){
-        //         subStrTail = min(subStrTail, idx - left + 1);
-        //         subStrHead = left;
-        //         if(hashArr[s[left]] == 0){ // making the substring shorter
-        //             validator++;
-        //         }
-        //         hashArr[s[left]]++; left++;
-        //     }
-        // }
-        // return subStrTail == INT_MAX? "" : s.substr(subStrHead, subStrTail);
+
+        int left = 0, counter = t.size();
+        int subStrHead = 0, subStrTail = INT_MAX, strSize = s.size();
+        for (int idx = 0; idx < strSize; idx++) {
+            if (hashArr[s[idx]] > 0) {
+                counter--;
+            }
+            hashArr[s[idx]]--;
+            while (counter == 0) {
+                if (subStrTail > idx - left + 1) {
+                    subStrTail = idx - left + 1;
+                    subStrHead = left;
+                }
+                hashArr[s[left]]++;
+                if (hashArr[s[left]] > 0) {
+                    counter++;
+                }
+                left++;
+            }
+        }
+        return subStrTail == INT_MAX ? "" : s.substr(subStrHead, subStrTail);
     }
 };
