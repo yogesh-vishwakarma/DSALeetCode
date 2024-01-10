3d598acac2ce6c457f3529e7f3d74512484e77ee
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-01-10 17:51:52 +0530

    Time: 19 ms (70.83%), Space: 22.6 MB (93.12%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        /* XOR approach */
+            // int res(0);
+            // for(auto itr : nums)
+            //     res = res^itr;
+            // return res;
+        
+            /*Binary Search*/
+            int low = 0, high = n - 2;
+        
+            while(low<=high){
+                int mid  = (low + high)/2;
+                if(nums[mid] == nums[mid^1])
+                    low = mid+1;
+                else
+                    high =  mid -1;
+            }
+            return nums[low];
+        return 0;
+    }
+};
