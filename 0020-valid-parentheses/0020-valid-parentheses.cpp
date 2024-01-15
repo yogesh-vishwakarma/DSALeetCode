31b261a2fdd442db7f2fbfe98a47209458f4bdba
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-01-15 15:48:55 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (38.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    
+    bool isValid(string s) {
+        if(s.size()%2 != 0)
+            return false;
+        unordered_map<char,char> mp;
+        mp['[']=']'; mp['{']='}'; mp['(']=')';
+        
+        stack<char> st;
+        for(auto el :  s ){
+            if(el == '(' || el == '{' || el == '[')
+                st.push(el);
+            else{
+                if(!st.empty() && el == mp[st.top()])
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return (st.empty())?  true : false;
+    }
+};
