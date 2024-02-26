20cce7f0f49d9ee254609e10880f9509a4b4faf4
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-02-26 17:01:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (31.44%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int cnt = 0;
-        for (int i = 0, n = nums.size(); i < n; ++i) {
-            cnt += nums[i] > (nums[(i + 1) % n]);
-        }
-        return cnt <= 1;
-    }
     // bool check(vector<int>& nums) {
-    //     int breakPoints = 0, n=nums.size();
-    //     for(int i=0; i < n; i++){
-    //         if(nums[i] > nums[(i+1)%n]){
-    //             breakPoints++;
-    //         }
+    //     int cnt = 0;
+    //     for (int i = 0, n = nums.size(); i < n; ++i) {
+    //         cnt += nums[i] > (nums[(i + 1) % n]);
     //     }
-    //     return breakPoints <= 1 ? true : false;
+    //     return cnt <= 1;
     // }
+    bool check(vector<int>& nums) {
+        int breakPoints = 0, n=nums.size();
+        for(int i=1; i < n ; i++){
+            if(nums[i-1] > nums[i]){
+                breakPoints++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            breakPoints++;
+        }
+        return breakPoints <= 1 ? true : false;
+    }
 };
