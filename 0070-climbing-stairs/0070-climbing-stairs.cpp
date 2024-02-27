b8f8558267030034a04a549f2b334e0c49563249
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-02-27 07:15:44 +0530

    Time: 2 ms (46.92%), Space: 7.1 MB (23.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,7 +4,7 @@ public:
         int lastWayCount = 1, ans = 1;
         for(int i = 2; i<= n;i++){
             ans = lastWayCount + ans;
-            lastWayCount = ans - lastWayCount;
+            lastWayCount = ans - lastWayCount;// to get old ans val as it's modified in above line
         }
         return ans;
     }
