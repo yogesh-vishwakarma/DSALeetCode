7a7b40664cd748ab58d60d35db69c7d760564af7
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-03-05 09:01:15 +0530

    Time: 55 ms (5.17%), Space: 13.9 MB (70.34%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int prefixP = 0, suffixP = s.size() -1;
-        while(prefixP < suffixP){
+        int prefixP = 0, suffixP = s.size() - 1;
+        while (prefixP < suffixP && s[prefixP] == s[suffixP]) {
             cout << prefixP << suffixP << endl;
-            if(s[prefixP] != s[suffixP]){
-                break;
-            }
-            while(prefixP + 1 < suffixP && s[prefixP] == s[prefixP + 1]) prefixP++;
-            while(suffixP - 1 > prefixP && s[suffixP] == s[suffixP - 1]) suffixP--;
-            prefixP++; suffixP--;
+            while (prefixP + 1 < suffixP && s[prefixP] == s[prefixP + 1])
+                prefixP++;
+            while (suffixP - 1 > prefixP && s[suffixP] == s[suffixP - 1])
+                suffixP--;
+            prefixP++;
+            suffixP--;
         }
         return suffixP - prefixP + 1;
     }
