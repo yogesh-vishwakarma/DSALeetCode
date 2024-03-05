4a533690fcbc95e322a0e3823705d7446a5aa62a
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-03-05 08:41:53 +0530

    Time: 11 ms (50.75%), Space: 16.1 MB (34.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,12 +3,12 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rowCount = matrix.size(), colCount = matrix[0].size();
         bool ifFirstRowZero = false;
-        for(int currRow = 0; currRow < rowCount; currRow++){
-            for(int currCol = 0; currCol < colCount; currCol++){
-                if(matrix[currRow][currCol] == 0){
-                    if(currRow == 0){
+        for (int currRow = 0; currRow < rowCount; currRow++) {
+            for (int currCol = 0; currCol < colCount; currCol++) {
+                if (matrix[currRow][currCol] == 0) {
+                    if (currRow == 0) {
                         ifFirstRowZero = true;
-                    }else{
+                    } else {
                         matrix[0][currCol] = 0;
                         matrix[currRow][0] = 0;
                     }
@@ -16,17 +16,16 @@ public:
             }
         }
 
-
-        for(int currRow = 1; currRow < rowCount; currRow++){
-            for(int currCol = 1; currCol < colCount; currCol++){
-                // if(matrix[0][currCol] == 0 || matrix[currRow][0] == 0){
-                //     matrix[currRow][currCol] = 0;
-                // }
-                matrix[currRow][currCol] = !matrix[0][currCol] || !matrix[currRow][0]? 0: matrix[currRow][currCol];
+        for (int currRow = 1; currRow < rowCount; currRow++) {
+            for (int currCol = 1; currCol < colCount; currCol++) {
+                matrix[currRow][currCol] =
+                    !matrix[0][currCol] || !matrix[currRow][0]
+                        ? 0
+                        : matrix[currRow][currCol];
             }
         }
 
-        if (matrix[0][0] == 0) { //matrix[0][0] is for first Column zero
+        if (matrix[0][0] == 0) { // matrix[0][0] is for first Column zero
             for (int j = 0; j < rowCount; j++) {
                 matrix[j][0] = 0;
             }
