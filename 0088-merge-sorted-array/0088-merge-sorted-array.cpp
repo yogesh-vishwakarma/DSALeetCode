edc66b8bbe0e9ad352b70a1c80ba49fb77a7a5ec
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-03-20 22:32:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    /*
-    Here we are replacing ending zeros with biggest elements from both arrays.
-    */
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1, j = n - 1, curr = m + n - 1;
-        while(j >= 0 ){
-            nums1[curr--] = (i>=0 &&  nums1[i] > nums2[j] ) ? nums1[i--] : nums2[j--];
+        int endPtr(m+n), p1 = m-1, p2 = n-1;
+        for(int i = m + n -1; i>=0; i--){
+            cout << nums1[i] <<" ";
+            nums1[i] = nums1[p1] > nums2[p2]? nums1[p1--] : nums2[p2--];
         }
     }
 };
