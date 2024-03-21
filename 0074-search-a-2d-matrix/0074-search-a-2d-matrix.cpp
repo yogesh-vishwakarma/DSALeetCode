9996dfd947f3a2fe9eeaca23f65e6bdab4920943
yogesh vishwakarma <42703881+yogesh-vishwakarma@users.noreply.github.com>
2024-03-21 09:11:55 +0530

    Time: 3 ms (67.55%), Space: 12.1 MB (10.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int currRow = matrix.size() - 1, currCol = matrix[0].size() - 1;
         while (currRow >= 0 && currCol >= 0) {
             if (matrix[currRow][currCol] == target) {
